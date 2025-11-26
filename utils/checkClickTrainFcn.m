d7157c1de48d006f7c083eef6409427c2046fc6a
spring <11815013@zju.edu.cn>
2025-11-26 09:57:51 +0800

    update 2025-11-26 09:57:51 by administrator

--- utils/checkClickTrainFcn.m
+++ utils/checkClickTrainFcn.m
@@ -1,4 +1,12 @@
 function soundParse = checkClickTrainFcn(rootpath, thr, minInterval)
+narginchk(1, 3);
+if nargin < 2
+    thr = 0.05;
+end
+
+if nargin < 3
+    minInterval = 1; % ms
+end
 files = dir([char(rootpath), '\*.wav']);
 [soundWave, fs] = cellfun(@(x) audioread(fullfile(rootpath, x)), {files.name}', "UniformOutput", false);
 T = cellfun(@(x, sound) 1/x:1/x:length(sound)/x, fs, soundWave, "UniformOutput", false);
@@ -6,6 +14,6 @@ T = cellfun(@(x, sound) 1/x:1/x:length(sound)/x, fs, soundWave, "UniformOutput",
 ICI = cellfun(@diff, clickT, "UniformOutput", false);
 soundLength = cellfun(@(x) x(end), clickT, "UniformOutput", false);
 
-soundParse = cell2struct([{files.name}', soundWave, fs, clickT, ICI, soundLength], ...
-                         ["name", "y1", "fs", "clickT", "ICI", "soundLength"], 2);
+soundParse = cell2struct([{files.name}',soundWave, T,  fs, clickT, ICI, soundLength], ...
+                         ["name", "y1", "T", "fs", "clickT", "ICI", "soundLength"], 2);
 end
