507c0e221c1985134001da1e59dce143575c43b1
Peirun Song <11815013@zju.edu.cn>
2026-04-15 21:32:46 +0800

    update 2026-04-15 21:32:46 by lenovo

--- updateSoundGenToolbox.m
+++ updateSoundGenToolbox.m
@@ -1,6 +1,13 @@
-function updateSoundGenToolbox(SyncOption)
-narginchk(0, 1);
+function updateSoundGenToolbox(SyncOption, remote)
+narginchk(0, 2);
 if nargin < 1
     SyncOption = false;
 end
-mu.syncRepositories([], "RepositoryPaths", fileparts(mfilename("fullpath")), "SyncOption", SyncOption);
+if nargin < 2
+    remote = 'origin';
+end
+mu.syncRepositories(...
+    "log", '', ...
+    "RepositoryPaths", fileparts(mfilename("fullpath")), ...
+    "SyncOption", SyncOption, ...
+    "Remote", remote);
